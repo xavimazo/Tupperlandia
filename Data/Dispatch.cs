78ab8b680c967fd91f83129ce31aa2dc104f492b
xavimazo <34321000+xavimazo@users.noreply.github.com>
2018-06-09 19:24:04 -0300

    installing packages

---
+++ Data/Dispatch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Dispatch
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Porcentaje de descuento")]
+        public string DispatchType { get; set; }
+    }
+}
